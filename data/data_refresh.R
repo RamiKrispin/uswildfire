b86736ec5f2c62e304a496d409d71b433594f156
RamiKrispin <ramkrisp@umich.edu>
2020-08-30 05:23:26 -0700

    update the dashboard

--- data/data_refresh.R
+++ data/data_refresh.R
@@ -1,13 +1,22 @@
 # Pulling geometry data from National Interagency Fire Center 
 # API ArcGIS
+# Website https://data-nifc.opendata.arcgis.com
+
 `%>%` <- magrittr::`%>%`
+
 cmd <- "curl 'https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Public_Wildfire_Perimeters_View/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'"
-
+json <- raw <- poly <- NULL
 json <- system(command = cmd, intern = TRUE)
-rm(json)
-raw <- jsonlite::fromJSON(json)
 
-length(raw$features$geometry$rings)
+if(is.null(json) || !is.character(json)){
+  stop("Could not pull the json file")
+}
+raw <- jsonlite::fromJSON(json)
+rm(json)
+
+if(is.null(raw) || length(raw$features$geometry$rings) == 0){
+  stop("Could not parse the json file")
+}
 
 
 poly <- lapply(seq_along(raw$features$geometry$rings), function(i){
@@ -22,8 +31,8 @@ poly <- lapply(seq_along(raw$features$geometry$rings), function(i){
       poly_temp <- x %>% as.data.frame() %>%
         stats::setNames(c("long", "lat")) %>%
         sp::Polygon()
-    df_temp$polygon <- list(poly_temp)  
-    return(df_temp)
+      df_temp$polygon <- list(poly_temp)  
+      return(df_temp)
       
     }) %>% dplyr::bind_rows()
   } else if(class(raw$features$geometry$rings[[i]]) == "array"){
@@ -39,16 +48,9 @@ poly <- lapply(seq_along(raw$features$geometry$rings), function(i){
   return(p)
 }) %>%
   dplyr::bind_rows()
-
-save(poly, file = "./data/us_wildfire_poly.RData")
-
-
-
-
-
-
-
-
-
-
+if(is.null(poly) || nrow(poly) == 0){
+  stop("Could not create the data")
+} else{
+  save(poly, file = "./data/us_wildfire_poly.RData")
+}
 
