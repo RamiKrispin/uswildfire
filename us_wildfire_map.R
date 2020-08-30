f0c64879efd94d5e1e3e1140ee341785d36861f1
RamiKrispin <ramkrisp@umich.edu>
2020-08-29 21:45:08 -0700

    update the docs

--- us_wildfire_map.R
+++ us_wildfire_map.R
@@ -1,14 +1,18 @@
+load("./data/wildfire_poly.RData")
 m <- leaflet::leaflet() %>% 
-  leaflet::setView(lng = -112, lat = 39, zoom = 5) %>%
+  leaflet::setView(lng = -112, lat = 39, zoom = 6) %>%
   leaflet::addTiles()
 
 
-m
+
 for(i in 1:nrow(poly)){
   m <- m %>% leaflet::addPolygons(data = poly$polygon[i][[1]],
                                   color = "red",
-                                  popup = paste("IncidentName:",poly$IncidentName[i]))
+                                  popup = paste("IncidentName:",poly$IncidentName[i], "<br>",
+                                                "Number of Acres:", round(poly$GISAcres[i]), "<br>",
+                                                "Object ID:", poly$OBJECTID[i], "<br>",
+                                                "Map Method:", poly$MapMethod[i], "<br>"))
 }
 
 
-m
+save(m, file = "./data/us_wildfire_map.RData")
