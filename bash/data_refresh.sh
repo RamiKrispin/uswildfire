7ec5c47df68a99aa515312d555e5e19c3018a9a9
RamiKrispin <ramkrisp@umich.edu>
2020-08-30 05:24:56 -0700

    update the deployment

--- bash/data_refresh.sh
+++ bash/data_refresh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Rendering the dashboard..."
-
+Rscript -e "source('./data/data_refresh.R')"
+Rscript -e "source('./data/us_wildfire_map.R')"
 Rscript -e "rmarkdown::render_site()"
 
 if [[ "$(git status --porcelain)" != "" ]]; then
